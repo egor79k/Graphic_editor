3419ac52e247b082ab5f6557ce4b02bed9477b65
egor79k <egor79k@yandex.ru>
2020-11-18 12:05:51 +0300

    Event system

---
+++ graphics/include/event_system.hpp
@@ -0,0 +1,28 @@
+#ifndef _EVENT_SYSTEM_HPP_
+#define _EVENT_SYSTEM_HPP_
+
+class Event_system;
+
+#include <vector>
+#include "window.hpp"
+
+
+class Event_system
+{
+private:
+	static std::vector<Abstract_window *> attached_on_redraw;
+	static std::vector<Abstract_window *> attached_on_mouse_press;
+	static std::vector<Abstract_window *> attached_on_mouse_release;
+	static std::vector<Abstract_window *> attached_on_mouse_move;
+
+public:
+	static void dispatch_event ();
+
+	static void attach_redraw (Abstract_window *window);
+	static void attach_mouse_press (Abstract_window *window);
+	static void attach_mouse_release (Abstract_window *window);
+	static void attach_mouse_move (Abstract_window *window);
+};
+
+
+#endif // _EVENT_SYSTEM_HPP_
---
+++ graphics/source/event_system.cpp
@@ -0,0 +1,71 @@
+#include "../include/event_system.hpp"
+
+
+//=============================================================================
+// ::::  Event_system  ::::
+//=============================================================================
+
+std::vector<Abstract_window *> Event_system::attached_on_redraw;
+std::vector<Abstract_window *> Event_system::attached_on_mouse_press;
+std::vector<Abstract_window *> Event_system::attached_on_mouse_release;
+std::vector<Abstract_window *> Event_system::attached_on_mouse_move;
+//-----------------------------------------------------------------------------
+
+void Event_system::dispatch_event ()
+{
+	for (auto window: attached_on_redraw)
+		window->handle_redraw ();
+
+	Event event;
+	while (Engine::poll_event (event))
+	{
+		switch (event.type)
+		{
+			case Event::Closed:
+				Engine::exit ();
+				break;
+
+			case Event::Mouse_pressed:
+				for (auto window: attached_on_mouse_press)
+					if (window->handle_mouse_press (event.mouse_click))
+						break;
+				break;
+
+			case Event::Mouse_released:
+				for (auto window: attached_on_mouse_release)
+					if (window->handle_mouse_release (event.mouse_click))
+						break;
+				break;
+
+			case Event::Mouse_moved:
+				for (auto window: attached_on_mouse_move)
+					if (window->handle_mouse_move (event.mouse_move))
+						break;
+				break;
+		}
+	}
+}
+//-----------------------------------------------------------------------------
+
+void Event_system::attach_redraw (Abstract_window *window)
+{
+	attached_on_redraw.push_back (window);
+}
+
+void Event_system::attach_mouse_press (Abstract_window *window)
+{
+	attached_on_mouse_press.push_back (window);
+}
+//-----------------------------------------------------------------------------
+
+void Event_system::attach_mouse_release (Abstract_window *window)
+{
+	attached_on_mouse_release.push_back (window);
+}
+//-----------------------------------------------------------------------------
+
+void Event_system::attach_mouse_move (Abstract_window *window)
+{
+	attached_on_mouse_move.push_back (window);
+}
+//=============================================================================
